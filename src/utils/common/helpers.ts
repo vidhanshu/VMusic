fdfa61cca2a600a70f25743dbceed7c759139d9c
vidhanshu <vidhanshu.borade.cs@ghrce.raisoni.net>
2024-01-24 15:14:10 +0530

    Fix shuffle

--- src/utils/common/helpers.ts
+++ src/utils/common/helpers.ts
@@ -54,9 +54,9 @@ export function shuffleArray<T>(array: T[]) {
   const newArray = [...array];
   for (let i = newArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j]!;
-    array[j] = temp!;
+    const temp = newArray[i];
+    newArray[i] = newArray[j]!;
+    newArray[j] = temp!;
   }
   return newArray;
 }
